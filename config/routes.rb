65bece052db52bf3901bf161f91ca621b6df6b5b
Paul Henke <paulmhenke@gmail.com>
2015-03-21 13:30:36 +0000

    Created posts controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-
-  get 'welcome/about'
+  resources :posts
   
-  get 'welcome/contact'
+  get 'about' => 'welcome#about'
   
   root to: 'welcome#index'
+  
+end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -61,4 +61,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
