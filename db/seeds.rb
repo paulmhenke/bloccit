9183a15338db4a02cb700c6ecc4ce1392ed42b47
Paul Henke <paulmhenke@gmail.com>
2015-03-31 21:54:57 +0000

    wip

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,15 @@
    user.save!
  end
  users = User.all
+
+# Create Topics
+ 15.times do
+   Topic.create!(
+     name:         Faker::Lorem.sentence,
+     description:  Faker::Lorem.paragraph
+   )
+ end
+ topics = Topic.all
  
  # Note: by calling `User.new` instead of `create`,
  # we create an instance of User which isn't immediately saved to the database.
@@ -24,6 +33,7 @@
  50.times do
    Post.create!(
      user: users.sample,
+     topic: topics.sample,
      title:  Faker::Lorem.sentence,
      body:   Faker::Lorem.paragraph
    )
@@ -39,12 +49,37 @@
    )
  end
 
-user = User.first
-user.skip_confirmation!
-user.skip_reconfirmation!
-user.update_attributes!(
-  email: 'paulmhenke@gmail.com',
-  password: 'helloworld')
+# Create an admin user
+ admin = User.new(
+   name:     'Admin User',
+   email:    'admin@example.com',
+   password: 'helloworld',
+   role:     'admin'
+ )
+ admin.skip_confirmation!
+admin.skip_reconfirmation!
+ admin.save!
+ 
+ # Create a moderator
+ moderator = User.new(
+   name:     'Moderator User',
+   email:    'moderator@example.com',
+   password: 'helloworld',
+   role:     'moderator'
+ )
+ moderator.skip_confirmation!
+moderator.skip_reconfirmation!
+ moderator.save!
+ 
+ # Create a member
+ member = User.new(
+   name:     'Member User',
+   email:    'member@example.com',
+   password: 'helloworld'
+ )
+ member.skip_confirmation!
+member.skip_reconfirmation!
+ member.save!
  
  puts "Seed finished"
  puts "#{Post.count} posts created"
