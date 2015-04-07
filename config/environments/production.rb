a1be21c8c7489b6afa960b3f11c63849a4f89491
Paul Henke <paulmhenke@gmail.com>
2015-04-07 01:05:24 +0000

    rename to paulhenke-bloccit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
    # http://bloccit.herokuapp.com
-  config.action_mailer.default_url_options = { host: 'bloccit.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'paulhenke-bloccit.herokuapp.com' }
 end
