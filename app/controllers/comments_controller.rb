c6a9594a5c41a89c242dfdf99e093c453f88e9cc
Paul Henke <paulmhenke@gmail.com>
2015-04-21 17:48:49 +0000

    complete comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,13 +7,13 @@ class CommentsController < ApplicationController
     # post.comments.create
     # associate the comment with a user
     # save the comment, and redirect to the post
-    
-    @post = Post.find(params[:post_id])
+    @topic = Topic.find(params[:topic_id])
+    @post = @topic.posts.find(params[:post_id])
     @comment = current_user.comments.build(comment_params)
     authorize @comment
       if @comment.save
         flash[:notice] = "Your comment was saved." 
-        redirect_to [@post]
+        redirect_to [@topic, @post]
       else
         flash[:error] = "There was a problem saving your comment.  Please try again."
         render :create
--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -11,7 +11,7 @@ class ApplicationPolicy
   end
 
   def show?
-    scope.where(:id => record.id).exists? || user.admin?
+    scope.where(:id => record.id).exists? || user = admin?
   end
 
   def create?
