2d00cd0c23b21a1d69def24734de36ea3a0d143b
Paul Henke <paulmhenke@gmail.com>
2015-05-02 02:19:32 +0000

    complete favoriting-assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -35,7 +35,7 @@ class Post < ActiveRecord::Base
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
   
-  validates :topic, presence:true
+  validates :topic, presence: true
   validates :user, presence: true
   
   def markdown_title
@@ -60,6 +60,4 @@ class Post < ActiveRecord::Base
      redcarpet = Redcarpet::Markdown.new(renderer, extensions)
      (redcarpet.render markdown).html_safe
   end
-  
-  
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,11 +4,25 @@ describe User do
   
   include TestFactories
   
+  before do
+    @user = authenticated_user
+    @post = Post.create(title: "rspec post", body: "this is an rspec post for the purpose of testing", id: 1)
+    @post_2 = Post.create(title: "second rspec post", body: "this is the second rspec post for testing", id: 2)
+  end
+    
   describe '#favorited(post)' do
-    xit "returns 'nil' if the user has not favorited the post" do
+    it "returns 'nil' if the user has not favorited the post" do
+      expect(@user.favorited(@post)).to be_nil
     end
     
-    xit "returns the appropriate favorite if it exists" do
+    it "returns the appropriate favorite if it exists" do
+      favorite = Favorite.create(post_id: @post.id, user_id: @user.id)
+      expect(@user.favorited(@post)).to eq(favorite)
+    end
+    
+    it "returns 'nil' if the user has favorited a different post" do
+      favorite = Favorite.create(post_id: @post.id, user_id: @user.id)
+      expect(@user.favorited(@post_2)).to be_nil
     end
   end
 end
