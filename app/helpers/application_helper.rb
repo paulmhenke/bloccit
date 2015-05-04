8b1ef256461bc4d8a24438b5a8dd06ececbf886f
Paul Henke <paulmhenke@gmail.com>
2015-05-04 21:37:22 +0000

    completed another interlude excercise (forgot to create branch)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,5 +7,12 @@ module ApplicationHelper
      end
    end
   
+  def up_vote_link_classes(post)
+    "glyphicon glyphicon-chevron-up #{(current_user.voted(post) && current_user.voted(post).up_vote?) ? 'voted' : '' }"
+  end
+  
+  def down_vote_link_classes(post)
+    "glyphicon glyphicon-chevron-down #{(current_user.voted(post) && current_user.voted(post).down_vote?) ? 'voted' : '' }"
+  end
  
 end
