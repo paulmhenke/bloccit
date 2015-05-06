206c598143cb1e9a8d7802c68ca3b629b4c70aa6
Paul Henke <paulmhenke@gmail.com>
2015-05-06 11:24:05 +0000

    completed public profiles assignment

--- spec/features/profiles_spec.rb
+++ spec/features/profiles_spec.rb
@@ -4,10 +4,13 @@ describe "Visiting profiles" do
   
   include TestFactories
   
+  include Warden::Test::Helpers
+  Warden.test_mode!
+  
   before do
     @user = authenticated_user
-    @post = associated_post(user: @user)
-    @comment = Comment.new(user: @user, body: "A comment")
+    @post = associated_post(user: @user, id: 1)
+    @comment = Comment.new(user: @user, body: "A comment", post_id: 1)
     allow(@comment).to receive(:send_favorite_emails)
     @comment.save!
   end
@@ -22,6 +25,21 @@ describe "Visiting profiles" do
       expect(page).to have_content(@post.title)
       expect(page).to have_content(@comment.body)
     end
+  end
+  
+  describe "visiting own profile" do
     
+    before do
+      login_as(@user, :scope => :user)
+    end
+    
+    it "shows profile" do
+      visit user_path(@user)
+      expect(current_path).to eq(user_path(@user))
+      
+      expect(page).to have_content(@user.name)
+      expect(page).to have_content(@post.title)
+      expect(page).to have_content(@comment.body)
+    end
   end
 end
